1e92ce92b34bd37ddc10222e445d50f62b62a437
amila <anjanaamila53@gmail.com>
2023-01-24 11:11:04 +0530

    updated

--- srilanka001/src/components/navbar.js
+++ srilanka001/src/components/navbar.js
@@ -4,7 +4,10 @@ import { Link } from 'react-router-dom';
 function Navbar() {
 
   const [click, setClick]= useState(false);
+
   const handelClick =() => setClick(!click);
+  const closeMobileMenu =() => setClick(false);
+
   return (
   
     <>
@@ -16,6 +19,28 @@ function Navbar() {
           <diV className="menu-icon" onClick={handelClick}>
             <i className={click ? 'fas fa-times' : 'fas fa-bars'} />
           </diV>
+          <ul className={click ? 'nav-menu active' : 'nav-menu'}>
+            <li className="nav-item">
+              <Link to="/" className= "nav-links" onClick={closeMobileMenu}>
+                Home
+              </Link>
+            </li>
+            <li className="nav-item">
+              <Link to="/Places" className= "nav-links" onClick={closeMobileMenu}>
+              Places
+              </Link>
+            </li>
+            <li className="nav-item">
+              <Link to="/Contact" className= "nav-links" onClick={closeMobileMenu}>
+                Contact
+              </Link>
+            </li>
+            <li className="nav-item">
+              <Link to="/Sing Up" className= "nav-links-mobile" onClick={closeMobileMenu}>
+                Sign Up
+              </Link>
+            </li>
+          </ul>
         </div>
       </nav> 
     </>
