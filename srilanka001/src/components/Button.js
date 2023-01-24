f00a7b4e90d3c45602d596c4e77122634b31a308
amila <anjanaamila53@gmail.com>
2023-01-24 11:27:52 +0530

    updated

---
+++ srilanka001/src/components/Button.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './Button.css';
+import { Link } from 'react-router-dom';
+
+export const Button =({children, 
+    type, 
+    onclick, 
+    buttonStyle, 
+    buttonSize
+}) => {
+    
+}
+
